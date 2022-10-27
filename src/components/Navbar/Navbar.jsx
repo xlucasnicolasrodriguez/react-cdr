cd71b3b04f03e2692ed9bd4083d7410978d59ce0
xlucasrodriguez <lucasxrodriguez@hotmail.com>
2022-10-26 23:52:04 -0300

    Brand

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,25 +1,25 @@
 import React from 'react';
-import './App.css'
+import './navbar.css'
 
-
-const App = () =>{
-    return(
-        <nav className="navbar navbar-expand-lg navbar-dark bg-danger">
+const Navbar = () => {
+    return (
+        <div>
+            <nav className="navbar navbar-expand-lg navbar-dark bg-danger">
             <div className="container-fluid">
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon" />
                 </button>
                 <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
-                <a className="navbar-brand" href="#">Hidden brand</a>
+                <a className="navbar-brand" href="#">Four Seasons Drawing</a>
                 <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                     <li className="nav-item">
-                    <a className="nav-link active" aria-current="page" href="#">Home</a>
+                    <a className="nav-link" aria-current="page" href="#">Home</a>
                     </li>
                     <li className="nav-item">
                     <a className="nav-link" href="#">Link</a>
                     </li>
                     <li className="nav-item">
-                    <a className="nav-link disabled">Disabled</a>
+                    <a className="nav-link" href="#">Link</a>
                     </li>
                 </ul>
                 <form className="d-flex" role="search">
@@ -29,9 +29,8 @@ const App = () =>{
                 </div>
             </div>
 </nav>
-
-    )
+        </div>
+    );
 }
 
-
-export default App
+export default Navbar;
